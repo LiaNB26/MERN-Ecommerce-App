e2661c022d2272b5fafc6786658efa481fa61eba
Lia <liabrachio@gmail.com>
2020-12-15 14:30:24 +0200

    backend: connecting to mongoDB and creating mongoose schemas

---
+++ backend/seeder.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import colors from "colors";
+
+import users from "./data/users.js";
+import products from "./data/products.js";
+import User from "./models/userModel.js";
+import Product from "./models/productModel.js";
+import Order from "./models/orderModel.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
+
+const importData = async () => {
+  try {
+    destroyData();
+
+    // inserting users dummy data
+    const createdUsers = await User.insertMany(users);
+
+    // getting the admin user's id and adding it to each product
+    const adminUser = createdUsers[0]._id;
+    const sampleProducts = products.map((product) => {
+      return {
+        ...product,
+        user: adminUser,
+      };
+    });
+
+    // inserting products dummy data
+    await Product.insertMany(sampleProducts);
+
+    console.log("Data Imported!".green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(`${err}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+const destroyData = async () => {
+  try {
+    // deleting existing data
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    console.log("Data Destroyed!".red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(`${err}`.red.inverse);
+    process.exit(1);
+  }
+};
